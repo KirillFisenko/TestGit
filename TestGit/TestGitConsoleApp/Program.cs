1b3d98101635b254f9008d47413733f24b4916c1
KirillFisenko <79265463469@ya.ru>
2023-01-25 18:10:18 +0300

    изменения

--- TestGit/TestGitConsoleApp/Program.cs
+++ TestGit/TestGitConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TestGitConsoleApp
 {
@@ -10,7 +11,38 @@ namespace TestGitConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Сделайте комит в удаленном репозитории и получите его в локальном репозитории.");
+            string[] s = Console.ReadLine().Split();
+
+            int n = Convert.ToInt32(s[0]);
+            int k = Convert.ToInt32(s[1]) - 1;
+
+            int[,] matrix = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] massive = Console.ReadLine().Split();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i,j] = Convert.ToInt32(massive[j]);
+                }
+            }
+
+            int countCalls = 0;
+            int numberPerson = 0;
+
+            for (int i = 0; i < n; i++)
+            {                
+                for (int j = 0; j < n; j++)
+                {
+                    if(matrix[i, k] > countCalls)
+                    {
+                        countCalls = matrix[i, j];
+                        numberPerson = i + 1;
+                    }
+                }
+            }
+
+            Console.WriteLine(numberPerson);
         }
     }
 }
