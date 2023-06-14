f098cf23fa22b8a6e10d89cae407941dc5582c92
KirillFisenko <79265463469@ya.ru>
2023-06-14 12:00:07 +0300

    +

---
+++ AppTest/ConsoleAppTest/forSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+var a = 1;
+Console.WriteLine(a);
+public class MainClass
+{
+    //public static void Main()
+    //{
+    //    var N = int.Parse(Console.ReadLine());
+    //    var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    //    BubbleSort(input, out int cnt);        
+    //    Console.WriteLine(cnt);
+    //}
+
+    //static void BubbleSort(int[] array, out int cnt)
+    //{
+    //    cnt = 0;
+    //    for (var i = array.Length - 1; i > 0; i--)
+    //    {
+    //        var flag = false;
+    //        for (var j = 0; j < i; j++)
+    //        {
+    //            if (array[j] > array[j + 1])
+    //            {
+    //                (array[j], array[j + 1]) = (array[j + 1], array[j]);
+    //                flag = true;
+    //                cnt++;
+    //            }
+    //        }
+    //        if (!flag)
+    //        {
+    //            return;
+    //        }
+    //    }
+        
+    //}
+}
