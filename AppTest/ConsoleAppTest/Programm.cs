4e282fa32adb03bf802a12e3fe377bd0e80f3ab8
KirillFisenko <79265463469@ya.ru>
2023-07-04 13:38:03 +0300

    #

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,41 +1,12 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
 
 public class MainClass
 {
     public static void Main()
     {
-        var N = int.Parse(Console.ReadLine());
-        var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-        InsertionSort(input);
-    }
-
-    static void InsertionSort(int[] array)
-    {
-        int buffer;
-        for (int i = 1; i < array.Length; i++)
-        {
-            var flag = false;
-            buffer = array[i]; // запоминаем в буфер элемент, который нужно вставить нужное место
-
-            int j = i; // индекс места, куда будем вставлять buffer
-
-            // пока не дошли до начала массива и очередной элемент больше буфера
-            while (j > 0 && array[j - 1] > buffer)
-            {
-                array[j] = array[j - 1]; // перемещаем больший элемент на одну позицию вправо
-                j--; // передвигаем индекс для просмотра элемента, который стоит левее
-                flag = true;
-            }
-
-            array[j] = buffer; // место найдено, вставить элемент
-            if (flag)
-            {
-                Console.WriteLine(string.Join(" ", array));
-                //safasf
-            }
-            
-        }
+        
     }
 }
