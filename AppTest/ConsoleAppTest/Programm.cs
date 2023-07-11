288b268a8f3aec046d493c8edaf1efc968f697a6
KirillFisenko <79265463469@ya.ru>
2023-07-11 22:00:24 +0300

    ##

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,52 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Runtime.Intrinsics.X86;
-
 public class MainClass
 {
 	public static void Main()
 	{
-		var n = Console.ReadLine();
-		var ballsColor = Console.ReadLine().Split().ToList();
-		var stack = new Stack<string>();
-		var kills = 0;
-		var currentColor = ballsColor[0];
-		ballsColor.RemoveAt(0);
-		stack.Push(currentColor);
-		var cnt = 1;
-		foreach (var collor in ballsColor)
+		var availableAccessesPath = new List<string>();
+
+		var N = int.Parse(Console.ReadLine());
+		for (int i = 0; i < N; i++)
 		{
-
-			if (collor == stack.Peek())
-			{
-				cnt++;
-			}
-			else
-			{
-				if(cnt >= 3)
-				{
-					kills += cnt;					
-					for (var i = 0; i < cnt; i++)
-					{
-						stack.Pop();
-					}
-					cnt = 1;					
-				}
-				if(stack.Count > 0)
-				{
-					if (collor == stack.Peek())
-					{
-						cnt++;
-					}
-				}
-			}
-
-			stack.Push(collor);
+			var availableAccessPath = Console.ReadLine();
+			availableAccessesPath.Add(availableAccessPath);
 		}
 
+		var M = int.Parse(Console.ReadLine());
+		for (int i = 0; i < M; i++)
+		{
+			var userRequestPath = Console.ReadLine();
 
-		Console.WriteLine(kills);
+			else
+			{
+				Console.WriteLine(availableAccessesPath.Any(
+				path => path.Split('/')
+				.ToHashSet()
+				.IsSubsetOf(userRequestPath.Split('/').ToHashSet())
+				) ? "YES" : "NO");
+			}			
+		}
 	}
 }
