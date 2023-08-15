58c23761a24ec9ce594d968d6f230ef50e44b0c7
KirillFisenko <79265463469@ya.ru>
2023-08-15 10:09:27 +0300

    #

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,32 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+
 public class MainClass
 {
 	public static void Main()
 	{
-		var availableAccessesPath = new List<string>();
-
-		var N = int.Parse(Console.ReadLine());
-		for (int i = 0; i < N; i++)
-		{
-			var availableAccessPath = Console.ReadLine();
-			availableAccessesPath.Add(availableAccessPath);
-		}
-
-		var M = int.Parse(Console.ReadLine());
-		for (int i = 0; i < M; i++)
-		{
-			var userRequestPath = Console.ReadLine();
-
-			else
-			{
-				Console.WriteLine(availableAccessesPath.Any(
-				path => path.Split('/')
-				.ToHashSet()
-				.IsSubsetOf(userRequestPath.Split('/').ToHashSet())
-				) ? "YES" : "NO");
-			}			
-		}
-	}
+		
+	}	
 }
