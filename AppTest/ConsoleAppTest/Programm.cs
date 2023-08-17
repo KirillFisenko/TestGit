7656d49b983901b29e219856ca845fa5b870c1de
KirillFisenko <79265463469@ya.ru>
2023-08-17 13:26:57 +0300

    #

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class MainClass
 {
 	public static void Main()
 	{
-		
+		var inputLine = Console.ReadLine().Split().ToList();
+		inputLine.ForEach(x => Console.WriteLine(x.Reverse()));
 	}	
 }
