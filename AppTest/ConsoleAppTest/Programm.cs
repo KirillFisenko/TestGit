cc568b77eb36a35cceca554191f1d5495416558b
KirillFisenko <79265463469@ya.ru>
2023-08-30 12:24:38 +0300

    #

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -5,8 +5,22 @@ using System.Collections.Generic;
 public class MainClass
 {
     public static void Main()
-    {     
+    {
+        var ticketCost = int.Parse(Console.ReadLine());
+        var windowSeatCost = int.Parse(Console.ReadLine());
+        var food = int.Parse(Console.ReadLine());
+        var m = int.Parse(Console.ReadLine());
 
-    
-    }​
+        if(m <= 3)
+        {
+            m = 0;
+        }
+        else
+        {
+            m = 200 * (m - 3);
+        }
+        
+        var result = ticketCost + windowSeatCost + food + m;
+        Console.WriteLine($"Полёт обойдётся в {result} рублей");
+    }
 }
