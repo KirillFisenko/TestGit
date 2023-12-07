65c44b891279846446c5ccaecb1373d822ee0c32
KirillFisenko <79265463469@ya.ru>
2023-12-07 18:38:13 +0300

    #

--- Deploy-f/Deploy-f/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Deploy_f.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
