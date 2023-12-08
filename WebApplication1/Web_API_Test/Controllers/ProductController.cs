0e59aba98439ec906af235edb782cbcadcedb187
KirillFisenko <79265463469@ya.ru>
2023-12-08 10:25:53 +0300

    testAPI

---
+++ WebApplication1/Web_API_Test/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Test.Models;
+using Web_API_Test.Services;
+
+namespace Web_API_Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return ProductService.GetAll();
+        }
+
+       
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return ProductService.GetById(id);
+        }
+
+        
+        [HttpPost]
+        public void Post(Product product)
+        {
+            ProductService.Add(product);
+        }
+
+       
+        [HttpPut("{id}")]
+        public void Put(Product product, int id)
+        {
+            ProductService.Update(product, id);
+        }
+
+       
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ProductService.Delete(id);
+        }
+    }
+}
---
+++ WebApplication1/Web_API_Test/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using Web_API_Test.Models;
+
+namespace Web_API_Test.Services
+{
+    public class ProductService
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product { Id = 1, Name = "Product1", Price = 100},
+            new Product { Id = 2, Name = "Product2", Price = 540}
+        };
+
+        public static List<Product> GetAll()
+        {
+            return products;
+        }
+
+        public static Product GetById(int id)
+        {
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        public static void Update(Product product, int id)
+        {
+            var index = products.FindIndex(x => x.Id == id);
+            products[index] = product;
+        }
+
+        public static void Delete(int id)
+        {
+            var index = products.FindIndex(x => x.Id == id);
+            products.Remove(products[index]);
+        }
+    }
+}
