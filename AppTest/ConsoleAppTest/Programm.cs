efd6eb8217f9b7d33b247d9a8e217019ee6d594d
kfisenko <kfisenko@mcdsoft.ru>
2024-02-16 18:02:39 +0300

    #

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,16 +1,7 @@
 ﻿public class Program
 {
-	static int Do(int x)
-	{
-		static int Do(int x)
-		{
-			return x;
-		}
-		return x;
-	}
-	static void Main()
-	{
-		Console.WriteLine(Do(1));
-	}
-	//сделал task_1
+    private static void Main()
+	{         
+        
+    }	
 }
