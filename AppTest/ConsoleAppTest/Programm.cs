3b5339e21dcd9dd97f20b9e907d871145b2e89e6
kfisenko <kfisenko@mcdsoft.ru>
2024-02-16 18:03:18 +0300

    сделал task_1

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -3,15 +3,8 @@ using System.Linq;
 
 namespace LINQ
 {
-    static class Program
-    {
-        static void Main()
-        {
-            var number = Console.ReadLine().Select(char.GetNumericValue).OrderBy(x => x).ToArray();            
-            var minDigit = number[0];
-            var avgDigit = number[1];
-            var maxDigit = number[2];
-            Console.WriteLine(maxDigit - minDigit == avgDigit ? "Числов ыуинтересное" : "Число неинтересное2");
-        }
-    }
+    private static void Main()
+	{         
+        //сделал task_1
+    }	
 }
