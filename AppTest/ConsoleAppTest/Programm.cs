9eafa0b16b0a6059a6bf52dbe1b5bb94c592b64a
kfisenko <kfisenko@mcdsoft.ru>
2024-04-01 11:46:05 +0300

    #chery pick

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,7 +1,17 @@
-﻿public class Program
+﻿using System;
+using System.Linq;
+
+namespace LINQ
 {
-    private static void Main()
-	{ 
-        //task_1
-    }	
+    static class Program
+    {
+        static void Main()
+        {
+            var number = Console.ReadLine().Select(char.GetNumericValue).OrderBy(x => x).ToArray();            
+            var minDigit = number[0];
+            var avgDigit = number[1];
+            var maxDigit = number[2];
+            Console.WriteLine(maxDigit - minDigit == avgDigit ? "Число интересное" : "Число неинтересное");
+        }
+    }
 }
