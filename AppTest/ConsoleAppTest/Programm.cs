65f59f816553f194938c5b2e7d211a4fb94a3158
KirillFisenko <79265463469@ya.ru>
2024-11-13 14:18:06 +0300

    +

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,97 +1,42 @@
 ﻿//::c#
 //::code
-public class Program
+
+public class UsersService
 {
-    public static void Main()
+    /// <summary>
+    /// Получение пользователя из таблицы users
+    /// </summary>
+    /// <param name="fullName">Полное имя пользлователя</param>
+    /// <returns>User</returns>
+    public static User Get(string fullName)
     {
-        bool continueProgram = true;
-
-        while (continueProgram)
+        var user = new User();
+        using var connection = new MySqlConnection(Constant.ConnectionString);
+        connection.Open();
+        var query = @"SELECT * FROM users
+                      WHERE full_name = @FullName AND is_active = 1;";
+        using var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue("@FullName", fullName);
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
         {
-            Console.WriteLine(@"
-************************************************
-* Добро пожаловать на онлайн платформу Stepik! *
-************************************************
-
-Выберите действие (введите число и нажмите Enter):
-
-1. Зарегистрироваться
-2. Закрыть приложение
-
-************************************************
-");
-
-            string choice = Console.ReadLine();
-
-            switch (choice)
-            {
-                case "1":
-                    RegisterUser();
-                    break;
-                case "2":
-                    Console.WriteLine("До свидания!");
-                    continueProgram = false;
-                    break;
-                default:
-                    Console.WriteLine("Неверный выбор. Попробуйте снова.");
-                    break;
-            }
+            user.FullName = reader.IsDBNull(1) ? null : reader.GetString(1);
+            user.Details = reader.IsDBNull(2) ? null : reader.GetString(2);
+            user.JoinDate = reader.GetDateTime(3);
+            user.Avatar = reader.IsDBNull(4) ? null : reader.GetString(4);
+            user.IsActive = reader.GetBoolean(5);
         }
-    }
 
-
-    public static void RegisterUser()
-    {
-        Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
-        var userName = Console.ReadLine();
-        var newUser = new User()
-        {
-            FullName = userName
-        };
-
-        var isAdditionSuccessful = UsersService.Add(newUser);
-
-        if (isAdditionSuccessful)
-        {
-            Console.WriteLine($"Пользователь '{newUser.FullName}' успешно добавлен.\n");
-        }
-        else
-        {
-            Console.WriteLine($"Произошла ошибка, произведен выход на главную страницу\n");
-        }
+        return user;
     }
 }
 
 //::header
 //using System;
-//using System.Collections.Generic;
+//using System.Reflection.PortableExecutable;
 
 
 //::footer
-public class UsersService
-{
-    /// <summary>
-    /// Добавление нового пользователя в таблицу users
-    /// </summary>
-    /// <param name="user">Новый пользователь</param>
-    /// <returns>Удалось ли добавить пользователя</returns>
-    public static bool Add(User user)
-    {
-        using var connection = new MySqlConnection(Constant.ConnectionString);
-        connection.Open();
-        var query = @"
-                INSERT INTO users (full_name, details, join_date, avatar, is_active)
-                VALUES (@FullName, @Details, @JoinDate, @Avatar, @IsActive)";
-        using var command = new MySqlCommand(query, connection);
-        command.Parameters.AddWithValue("@FullName", user.FullName);
-        command.Parameters.AddWithValue("@Details", user.Details);
-        command.Parameters.AddWithValue("@JoinDate", user.JoinDate);
-        command.Parameters.AddWithValue("@Avatar", user.Avatar);
-        command.Parameters.AddWithValue("@IsActive", user.IsActive);
-        var rowsAffected = command.ExecuteNonQuery();
-        return rowsAffected == 1;
-    }
-}
 
 public class MySqlConnection : IDisposable
 {
@@ -106,6 +51,38 @@ public class MySqlConnection : IDisposable
     public void Dispose()
     {
         WasDisposeCalled = true;
+    }
+}
+
+public sealed class MySqlDataReader : IDisposable
+{
+    public MySqlCommand Command { get; set; }
+
+    public void Dispose() { }
+
+    public bool GetBoolean(int v)
+    {
+        return true;
+    }
+
+    public DateTime GetDateTime(int v)
+    {
+        return DateTime.UtcNow;
+    }
+
+    public string GetString(int v)
+    {
+        return "";
+    }
+
+    public bool IsDBNull(int v)
+    {
+        return false;
+    }
+
+    public bool Read()
+    {
+        return false;
     }
 }
 
@@ -127,6 +104,8 @@ public class MySqlCommand : IDisposable
 {
     public static bool WasExecuteNonQueryCalled = false;
     public static bool WasDisposeCalled = false;
+    public static bool WasExecuteReaderCalled = false;
+
     public new MySqlParameter Parameters { get; } = new MySqlParameter("", "");
     public string CommandText { get; internal set; }
     public MySqlCommand(string cmdText, MySqlConnection connection) { }
@@ -136,6 +115,13 @@ public class MySqlCommand : IDisposable
         WasExecuteNonQueryCalled = true;
         return 1;
     }
+
+    public MySqlDataReader ExecuteReader()
+    {
+        WasExecuteReaderCalled = true;
+        return new MySqlDataReader();
+    }
+
     public void Dispose()
     {
         WasDisposeCalled = true;
@@ -155,4 +141,22 @@ public class User
     public DateTime JoinDate { get; set; } = DateTime.Now;
     public string? Avatar { get; set; }
     public bool IsActive { get; set; } = true;
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var result = UsersService.Get("");
+        Console.WriteLine(string.IsNullOrEmpty(result.FullName));
+        Console.WriteLine(string.IsNullOrEmpty(result.Details));
+        Console.WriteLine(result.JoinDate.Date == DateTime.Today.Date);
+        Console.WriteLine(string.IsNullOrEmpty(result.Avatar));
+        Console.WriteLine(result.IsActive);
+
+        Console.WriteLine(MySqlConnection.WasOpenCalled);
+        Console.WriteLine(MySqlConnection.WasDisposeCalled);
+        Console.WriteLine(MySqlCommand.WasDisposeCalled);
+    }
 }
