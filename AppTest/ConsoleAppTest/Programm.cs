3310a2fb0602469348b6671250d3e549aa4171b8
KirillFisenko <79265463469@ya.ru>
2024-11-22 14:40:56 +0300

    +

--- AppTest/ConsoleAppTest/Programm.cs
+++ AppTest/ConsoleAppTest/Programm.cs
@@ -1,71 +1,27 @@
 ﻿::c#
 ::code
 
-    public static void Main()
+/// <summary>
+/// Получение общего количества пользователей
+/// </summary>
+public static int GetTotalCount()
 {
-    bool continueProgram = true;
+    using var connection = new MySqlConnection(Constant.ConnectionString);
+    connection.Open();
 
-    while (continueProgram)
-    {
-        Console.WriteLine(@"
-************************************************
-* Добро пожаловать на онлайн платформу Stepik! *
-************************************************
+    var query = "SELECT COUNT(*) FROM users;";
 
-Выберите действие (введите число и нажмите Enter):
+    using var command = new MySqlCommand(query, connection);
+    var result = command.ExecuteScalar();
 
-1. Зарегистрироваться
-2. Закрыть приложение
-
-************************************************
-");
-
-        string choice = Console.ReadLine();
-
-        switch (choice)
-        {
-            case "1":
-                RegisterUser();
-                break;
-            case "2":
-                Console.WriteLine("До свидания!");
-                continueProgram = false;
-                break;
-            default:
-                Console.WriteLine("Неверный выбор. Попробуйте снова.");
-                break;
-        }
-    }
+    return result != null ? Convert.ToInt32(result) : 0;
 }
 
 ::header
 using System;
 using System.Collections.Generic;
 
-public class UsersService
-{
-    /// <summary>
-    /// Добавление нового пользователя в таблицу users
-    /// </summary>
-    /// <param name="user">Новый пользователь</param>
-    /// <returns>Удалось ли добавить пользователя</returns>
-    public static bool Add(User user)
-    {
-        using var connection = new MySqlConnection(Constant.ConnectionString);
-        connection.Open();
-        var query = @"
-                INSERT INTO users (full_name, details, join_date, avatar, is_active)
-                VALUES (@FullName, @Details, @JoinDate, @Avatar, @IsActive)";
-        using var command = new MySqlCommand(query, connection);
-        command.Parameters.AddWithValue("@FullName", user.FullName);
-        command.Parameters.AddWithValue("@Details", user.Details);
-        command.Parameters.AddWithValue("@JoinDate", user.JoinDate);
-        command.Parameters.AddWithValue("@Avatar", user.Avatar);
-        command.Parameters.AddWithValue("@IsActive", user.IsActive);
-        var rowsAffected = command.ExecuteNonQuery();
-        return rowsAffected == 1;
-    }
-}
+
 
 public class MySqlConnection : IDisposable
 {
@@ -101,6 +57,7 @@ public class MySqlCommand : IDisposable
 {
     public static bool WasExecuteNonQueryCalled = false;
     public static bool WasDisposeCalled = false;
+    public static bool WasExecuteScalarCalled = false;
     public new MySqlParameter Parameters { get; } = new MySqlParameter("", "");
     public string CommandText { get; internal set; }
     public MySqlCommand(string cmdText, MySqlConnection connection) { }
@@ -110,6 +67,13 @@ public class MySqlCommand : IDisposable
         WasExecuteNonQueryCalled = true;
         return 1;
     }
+
+    public object ExecuteScalar()
+    {
+        WasExecuteScalarCalled = true;
+        return 10;
+    }
+
     public void Dispose()
     {
         WasDisposeCalled = true;
@@ -131,29 +95,20 @@ public class User
     public bool IsActive { get; set; } = true;
 }
 
-public class Program
+public class UsersService
 {
-::footer
+    ::footer
 
-        public static void RegisterUser()
+
+    public class Program
     {
-        Console.WriteLine("Введите имя и фамилию через пробел и нажмите Enter:");
-        var userName = Console.ReadLine();
-        var newUser = new User()
+        public static void Main()
         {
-            FullName = userName
-        };
-
-        var isAdditionSuccessful = UsersService.Add(newUser);
-
-        if (isAdditionSuccessful)
-        {
-            Console.WriteLine($"Пользователь '{newUser.FullName}' успешно добавлен.\n");
-        }
-        else
-        {
-            Console.WriteLine($"Произошла ошибка, произведен выход на главную страницу\n");
+            UsersService.GetTotalCount();
+            Console.WriteLine(MySqlConnection.WasDisposeCalled);
+            Console.WriteLine(MySqlConnection.WasOpenCalled);
+            Console.WriteLine(MySqlCommand.WasDisposeCalled);
+            Console.WriteLine(MySqlCommand.WasExecuteScalarCalled);
         }
     }
-
 }
